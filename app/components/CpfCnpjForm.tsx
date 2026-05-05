240f85dc588508e36b85254e641b61e5089d87fe
João Lucas Lourenço <118781114+JoaoLucasLourenco@users.noreply.github.com>
2026-05-05 01:20:49 +0000

    feat: estiliza form de validacao e verificao

--- app/components/CpfCnpjForm.tsx
+++ app/components/CpfCnpjForm.tsx
@@ -33,56 +33,103 @@ export default function CpfCnpjForm() {
     const result = ValidacaoDocumento.calcularDigitosCPF(cpfCalculoDigitos);
     setResultadoCalculoDigitosCpf(result);
   };
-
+  const handleLimpaCampos = () => {
+    setCpf("");
+    setCnpj("");
+    setCpfCalculoDigitos("");
+    setCnpjCalculoDigitos("");
+    setResultadoValidacaoCpf(null);
+    setResultadoValidacaoCnpj(null);
+    setResultadoCalculoDigitosCpf("");
+    setResultadoCalculoDigitosCnpj("");
+  };
   return (
     <>
-      <div className="gap-2">
-        <input value={cpf} onChange={(e) => setCpf(e.target.value)} />
-        <button onClick={handleValidarCPF}>Validar CPF</button>
-        <label>
-          {resultadoValidacaoCpf !== null && (
-            <p>{resultadoValidacaoCpf ? "CPF Válido ✓" : "CPF Inválido ✗"}</p>
-          )}
-        </label>
-        <input
-          value={cpfCalculoDigitos}
-          onChange={(e) => setCpfCalculoDigitos(e.target.value)}
-        />
-        <button onClick={handleCalculaDigitosCPF}>
-          Descobre dígitos verificadores CPF
-        </button>
-        <label>
-          {resultadoCalculoDigitosCpf !== "" && (
-            <p>{resultadoCalculoDigitosCpf}</p>
-          )}
-        </label>
+      <div className="flex flex-col columns-1 gap-2 m-6 justify-center items-center">
+        <div className="flex gap-2">
+          <input
+            className="border rounded-md p-2 focus:outline-slate-700"
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white py-2 px-6 rounded-full hover:cursor-pointer hover:bg-blue-700"
+            onClick={handleValidarCPF}
+          >
+            Validar CPF
+          </button>
+          <label>
+            {resultadoValidacaoCpf !== null && (
+              <p>{resultadoValidacaoCpf ? "CPF Válido ✓" : "CPF Inválido ✗"}</p>
+            )}
+          </label>
+        </div>
+        <div className="flex gap-2">
+          <input
+            className="border rounded-md p-2 focus:outline-slate-700"
+            value={cpfCalculoDigitos}
+            onChange={(e) => setCpfCalculoDigitos(e.target.value)}
+          />
+          <button
+            className="bg-sky-500 text-white py-2 px-6 rounded-full hover:cursor-pointer hover:bg-sky-700"
+            onClick={handleCalculaDigitosCPF}
+          >
+            Descobre dígitos verificadores CPF
+          </button>
+          <input
+            className="border rounded-md p-2 focus:outline-slate-700 w-10"
+            value={resultadoCalculoDigitosCpf}
+            readOnly
+          />
+        </div>
       </div>
-      <div className="">
-        <input
-          maxLength={14}
-          value={cnpj}
-          onChange={(e) => setCnpj(e.target.value)}
-        />
-        <button onClick={handleValidarCnpj}>Validar CNPJ</button>
-        <label>
-          {resultadoValidacaoCnpj !== null && (
-            <p>
-              {resultadoValidacaoCnpj ? "CNPJ Válido ✓" : "CNPJ Inválido ✗"}
-            </p>
-          )}
-        </label>
-        <input
-          value={cpfCalculoDigitos}
-          onChange={(e) => setCnpjCalculoDigitos(e.target.value)}
-        />
-        <button onClick={handleCalculaDigitosCNPJ}>
-          Descobre dígitos verificadores CNPJ
+      <div className="flex flex-col columns-1 gap-2 justify-center items-center">
+        <div className="flex gap-2">
+          <input
+            maxLength={14}
+            className="border rounded-md p-2 focus:outline-slate-700"
+            value={cnpj}
+            onChange={(e) => setCnpj(e.target.value)}
+          />
+          <button
+            className="bg-fuchsia-500 text-white py-2 px-6 rounded-full hover:cursor-pointer hover:bg-fuchsia-700"
+            onClick={handleValidarCnpj}
+          >
+            Validar CNPJ
+          </button>
+          <label>
+            {resultadoValidacaoCnpj !== null && (
+              <p>
+                {resultadoValidacaoCnpj ? "CNPJ Válido ✓" : "CNPJ Inválido ✗"}
+              </p>
+            )}
+          </label>
+        </div>
+        <div className="flex gap-2">
+          <input
+            maxLength={12}
+            className="border rounded-md p-2 focus:outline-slate-700"
+            value={cnpjCalculoDigitos}
+            onChange={(e) => setCnpjCalculoDigitos(e.target.value)}
+          />
+          <button
+            className="bg-purple-500 text-white py-2 px-6 rounded-full hover:cursor-pointer hover:bg-purple-700"
+            onClick={handleCalculaDigitosCNPJ}
+          >
+            Descobre dígitos verificadores CNPJ
+          </button>
+          <input
+            className="border rounded-md p-2 focus:outline-slate-700 w-10"
+            value={resultadoCalculoDigitosCnpj}
+            readOnly
+          />
+        </div>
+        <button
+          className=" text-slate-700 py-2 px-6 rounded-full hover:cursor-pointer hover:bg-purple-200"
+          onClick={handleLimpaCampos}
+        >
+          Limpar campos
         </button>
-        <label>
-          {resultadoCalculoDigitosCnpj !== "" && (
-            <p>{resultadoCalculoDigitosCnpj}</p>
-          )}
-        </label>
       </div>
     </>
   );
